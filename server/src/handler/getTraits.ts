e5707c27eacc1c8d2488e9e4c718faad89baefde
Reetik Rajan <r4reetik@users.noreply.github.com>
2023-12-09 13:06:57 +0530

    add: mine and want

--- server/src/handler/getTraits.ts
+++ server/src/handler/getTraits.ts
@@ -13,19 +13,9 @@ const getTraits = async (_req: Request, _res: Response) => {
 		return;
 	}
 
-	const traitsList = data[0].traits.split(',');
-
-	const traitsKV: { [key: string]: number } = traits.reduce(
-		(acc, trait) => {
-			acc[trait] = traitsList.includes(trait) ? 1 : 0;
-			return acc;
-		},
-		{} as { [key: string]: number }
-	);
-
-	data[0].traits = traitsKV;
-
-	_res.status(200).json(data[0]);
+	_res.status(200).json({
+		success: 'valid user',
+	});
 };
 
 export default getTraits;
